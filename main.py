485076a4f4b1c76d7be344878dd8bd952716c9e7
Corey F <95204233+fercor1@users.noreply.github.com>
2022-11-26 15:01:38 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+
+from marvel import Marvel
+
+# "Data provided by Marvel. © 2014 Marvel"
+
+m = Marvel(PUBLIC_KEY, PRIVATE_KEY)
+
+characters = m.characters
+comics = m.comics
+creators = m.creators
+events = m.events
+series = m.series
+stories = m.stories
+
+x = input("Type the name of a Marvel character: ")
+
+allChars = characters.all(nameStartsWith=x)["data"]["results"]
+
+for char in allChars:
+    print(char["name"], "=", char["id"])
+    #get every item in char sub comics sub 
+    for comics in char["comics"]["items"]:
+        print(comics["name"])
+    print("-----------------------------")
